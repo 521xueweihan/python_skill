c82dc4910c158b6cd357fb396499eef40bccc734
xueweihan <595666367@qq.com>
2015-06-06 22:14:53 +0800

    数据交换值的时候不推荐使用中间变量

--- 2/Proposal9.py
+++ 2/Proposal9.py
@@ -7,5 +7,5 @@ from timeit import Timer
 t1 = Timer('temp = x;x = y;y = temp', 'x = 2;y = 3').timeit()
 t2 = Timer('x, y = y, x', 'x = 2;y = 3').timeit()
 
-print t1 
-print t2 
+print "使用中间值，用时：", t1 
+print "不使用中间值，用时：", t2 
